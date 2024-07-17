f66c5184759b1842f38c364f55a5569c1cd61292
Надежда Лобзина <nadezda@MacBook-Air-Roman.local>
2024-07-17 14:12:54 +0300

    First try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function addCard(cardData, removeCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
-// @todo: Функция создания карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", removeCard);
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function removeCard(evt) {
+  const evtTarget = evt.target;
+  const listItem = evtTarget.closest(".places__item");
+  listItem.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (cardData) {
+  const cardElement = addCard(cardData, removeCard);
+  placesList.append(cardElement);
+});
